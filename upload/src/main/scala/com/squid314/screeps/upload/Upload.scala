56f48f2036a3ca83bd21bafca457b49bc7debac9
squid314 <squid314@users.noreply.github.com>
2020-04-11 01:03:49 -0600

    upload has longer timeout

--- upload/src/main/scala/com/squid314/screeps/upload/Upload.scala
+++ upload/src/main/scala/com/squid314/screeps/upload/Upload.scala
@@ -13,17 +13,18 @@ import scala.util.Using.resource
 case class Creds(accountAlias: String, token: String, ptr: Boolean)
 
 /** main class to upload the generated file */
-object Main extends App {
+object Upload extends App {
     implicit val credsFormat: JsonFormat[Creds] = jsonFormat3(Creds)
 
     // hacky git branch name extraction, but whatever
-    for (githead <- resource(Source.fromFile(".git/HEAD")) {_.getLines.toList} // toList required or the file closes
+    for (githead <- resource(Source.fromFile(".git/HEAD")) {_.getLines.toList}
         .find(_.startsWith("ref: refs/heads/"))
         .map(_.substring("ref: refs/heads/".length))) {
 
-        for (compiledjs <- List("./screeps/target/scala-2.13/screeps-opt.js",
-            "./screeps/target/scala-2.13/screeps-fastopt.js")
-            .find(f => Files.isRegularFile(Paths.get(f)))) {
+        for (compiledjs <-
+                 List("./screeps/target/scala-2.13/screeps-opt.js",
+                     "./screeps/target/scala-2.13/screeps-fastopt.js")
+                     .find(f => Files.isRegularFile(Paths.get(f)))) {
 
             println(s"uploading file: $compiledjs")
             val upload = Map(
@@ -36,6 +37,7 @@ object Main extends App {
             val creds = resource(Source.fromFile(".screeps.json")) {_.mkString}.parseJson.convertTo[Creds]
             println(s"uploading for branch: $githead")
             val resp = Http("https://screeps.com/api/user/code")
+                .timeout(2500, 5000)
                 .postData(upload.toJson.compactPrint)
                 .auth(creds.accountAlias, creds.token)
                 .header("content-type", "application/json")
